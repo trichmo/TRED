a7c4e6312b1ea8a814bec7594ac8bb259af58e38
Turner Richmond <trichmo@ncsu.edu>
2017-01-06 14:47:10 -0500

    Fixed tabbing

--- TopologyFunctionality/Octree/Octree.py
+++ TopologyFunctionality/Octree/Octree.py
@@ -3,88 +3,88 @@ from .OctreeBin import OctreeBin
 
 class Octree(object):
 
-	def __init__(self, minDepth):
-		self.firstLevel = []
-		self.bounds = []
-		self.points = []
-		self.minDepth = minDepth
-		
-	def appendPoints(self, points):
-                for point in points:
-                        addPointToBinPath(point)
-		self.points.extend(points)
-		removal =  self.points[0:len(points)]
-		for point in removal:
-                        bin = getBin(point.binPath[-1])
-                        bin.removePoints(point)
-                del self.points[0:len(points)]
-                        
-	
-	def prependPoints(self, points):
-                for point in points:
-                        addPointToBinPath(point)
-                removal = self.points[-len(points):]
-                for point in removal:
-                        bin = getBin(point.binPath[-1])
-                        bin.removePoints(point)
-                del self.points[-len(points):]
-                points.append(self.points)
-                self.points = points
-		
-	def createOctree(self, points, is2D):
-		self.points = points
-		minX = float('inf')
-		minY = float('inf')
-		minZ = float('inf')
-		maxX = float('-inf')
-		maxY = float('-inf')
-		maxZ = float('-inf')
-		for point in points:
-			if point.X < minX:
-				minX = point.X
-			if point.Y < minY:
-				minY = point.Y
-			if point.Z < minZ:
-				minZ = point.Z
-			if point.X > maxX:
-				maxX = point.X
-			if point.Y > maxY:
-				maxY = point.Y
-			if point.Z > maxZ:
-				maxZ = point.Z
-				
-		if is2D:
-			minZ = minZ-1
-			maxZ = maxZ+1
-		self.bounds = Bounds(minX,minY,minZ,maxX,maxY,maxZ)
-		self.firstLevel = OctreeBin(None, self.points, self.bounds,1)
-		self.splitBin(self.firstLevel)
-		
-	def splitBin(self,bin):
-		if len(bin.points) >= 2 and bin.depth < self.minDepth:
-			bin.divide()
-			for child in bin.children:
-				self.splitBin(child)
-	
-	def drawBins(self, bin):
-		if len(bin.children)==0:
-                        print(bin.bounds.minX,bin.bounds.minY,bin.bounds.maxX,bin.bounds.maxY,length(bin.points))
-                        return (bin.bounds.minX,bin.bounds.minY,bin.bounds.maxX,bin.bounds.maxY,length(bin.points))
-		else:
-                        binFacts = []
-                        for child in bin.children:
-                                binFacts.append(child.drawBins())
-                        return binFacts
+    def __init__(self, minDepth):
+        self.firstLevel = []
+        self.bounds = []
+        self.points = []
+        self.minDepth = minDepth
 
-        def getBin(self, binNos):
-                currBin = self.firstLevel
-                for binNo in binNos:
-                        currBin = currBin.children[binNo]
-                return currBin
+    def appendPoints(self, points):
+        for point in points:
+            addPointToBinPath(point)
+        self.points.extend(points)
+        removal =  self.points[0:len(points)]
+        for point in removal:
+            newBin = getBin(point.binPath[-1])
+            newBin.removePoints(point)
+        del self.points[0:len(points)]
 
-        def addPointToBinPath(self,point):
-                currBin = self.firstLevel
-                while len(currBin.children)~=0:
-                        idx = currBin.findIndex(point)
-                        point.addToBinPath([idx])
-                        currBin = currBin.children[idx]
+
+    def prependPoints(self, points):
+        for point in points:
+            addPointToBinPath(point)
+        removal = self.points[-len(points):]
+        for point in removal:
+            newBin = getBin(point.binPath[-1])
+            newBin.removePoints(point)
+        del self.points[-len(points):]
+        points.append(self.points)
+        self.points = points
+
+    def createOctree(self, points, is2D):
+        self.points = points
+        minX = float('inf')
+        minY = float('inf')
+        minZ = float('inf')
+        maxX = float('-inf')
+        maxY = float('-inf')
+        maxZ = float('-inf')
+        for point in points:
+            if point.X < minX:
+                minX = point.X
+            if point.Y < minY:
+                minY = point.Y
+            if point.Z < minZ:
+                minZ = point.Z
+            if point.X > maxX:
+                maxX = point.X
+            if point.Y > maxY:
+                maxY = point.Y
+            if point.Z > maxZ:
+                maxZ = point.Z
+
+        if is2D:
+            minZ = minZ-1
+            maxZ = maxZ+1
+        self.bounds = Bounds(minX,minY,minZ,maxX,maxY,maxZ)
+        self.firstLevel = OctreeBin(None, self.points, self.bounds,1)
+        self.splitBin(self.firstLevel)
+
+    def splitBin(self,newBin):
+        if len(newBin.points) >= 2 and newBin.depth < self.minDepth:
+            newBin.divide()
+            for child in newBin.children:
+                self.splitBin(child)
+
+    def drawBins(self, newBin):
+        if len(newBin.children)==0:
+            print(newBin.bounds.minX,newBin.bounds.minY,newBin.bounds.maxX,newBin.bounds.maxY,length(newBin.points))
+            return (newBin.bounds.minX,newBin.bounds.minY,newBin.bounds.maxX,newBin.bounds.maxY,length(newBin.points))
+        else:
+            binFacts = []
+            for child in newBin.children:
+                binFacts.append(child.drawBins())
+            return binFacts
+
+    def getBin(self, binNos):
+        currBin = self.firstLevel
+        for binNo in binNos:
+            currBin = currBin.children[binNo]
+        return currBin
+
+    def addPointToBinPath(self,point):
+        currBin = self.firstLevel
+        while len(currBin.children)!=0:
+            idx = currBin.findIndex(point)
+            point.addToBinPath([idx])
+            currBin = currBin.children[idx]
--- TopologyFunctionality/Octree/OctreeBin.py
+++ TopologyFunctionality/Octree/OctreeBin.py
@@ -1,49 +1,49 @@
 class OctreeBin(object):
 
-	#Children always stored so binary search works as:
-	#is greater than zMid, is greater than yMid, is greather than xMid
-	#if true, lies to right else to left, greater for all at index 7. Less for all at index 0
+    #Children always stored so binary search works as:
+    #is greater than zMid, is greater than yMid, is greather than xMid
+    #if true, lies to right else to left, greater for all at index 7. Less for all at index 0
 
-	def __init__(self, parent, points, bounds,depth):
-		self.children = []
-		self.points = points
-		self.parent = parent
-		self.bounds = bounds
-		self.depth = depth
-		
-		
-	def divide(self):
-		for i in range(8):
-			childBound = self.bounds.getChild(i)
-			pts=[]
-			newChild = OctreeBin(self,pts,childBound, self.depth+1)
-			self.children.append(newChild)
-		for point in self.points:
-			idx = self.findIndex(point)
-			self.children[idx].addPoints([point])
-			point.addToBinPath([idx])
-		del self.points[:]
-		
-	def mergeChildren(self):
-		for child in children:
-			for point in child.points:
-				point.shortenBinPath(1)
-			self.points.extend(child.points)
-			child.points = []
-		del self.children[:]
-	
-	def addPoints(self, points):
-		self.points.extend(points)
+    def __init__(self, parent, points, bounds,depth):
+        self.children = []
+        self.points = points
+        self.parent = parent
+        self.bounds = bounds
+        self.depth = depth
+                
+                
+    def divide(self):
+        for i in range(8):
+            childBound = self.bounds.getChild(i)
+            pts=[]
+            newChild = OctreeBin(self,pts,childBound, self.depth+1)
+            self.children.append(newChild)
+        for point in self.points:
+            idx = self.findIndex(point)
+            self.children[idx].addPoints([point])
+            point.addToBinPath([idx])
+        del self.points[:]
+                
+    def mergeChildren(self):
+        for child in children:
+            for point in child.points:
+                point.shortenBinPath(1)
+                self.points.extend(child.points)
+                child.points = []
+        del self.children[:]
 
-	def removePoint(self,point):
-                self.points.remove(point)
+    def addPoints(self, points):
+        self.points.extend(points)
 
-        def findIndex(self,point):
-                idx=0
-		if(point.Z > self.bounds.midZ):
-			idx=4
-		if(point.Y > self.bounds.midY):
-			idx = idx + 2
-		if(point.X > self.bounds.midX):
-			idx = idx + 1
-		return idx
+    def removePoint(self,point):
+        self.points.remove(point)
+
+    def findIndex(self,point):
+        idx=0
+        if(point.Z > self.bounds.midZ):
+            idx=4
+        if(point.Y > self.bounds.midY):
+            idx = idx + 2
+        if(point.X > self.bounds.midX):
+            idx = idx + 1
+        return idx
